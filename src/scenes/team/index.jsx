69677a9b63f72beb6994ecf24d62a55f2bf5109c
micleah <mchege78@gmail.com>
2023-01-20 01:22:03 +0300

    Added sidebar & header

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,23 @@
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+  ];
+};
